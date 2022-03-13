74da63add52788eb55f6079b4dbcb1642ee3a511
Vitor de Paula <vitordcode@gmail.com>
2022-03-13 16:42:41 -0300

    Finalizando app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,42 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+interface Task {
+  id: number
+  title: string
+  done: boolean
+}
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldTasks => [...oldTasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTasks = tasks.map(task => ({ ...task }))
+
+    const foundItem = updateTasks.find(item => item.id === id)
+
+    if (!foundItem) {
+      return
+    }
+
+    foundItem.done = !foundItem.done
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTask = tasks.filter(task => task.id !== id)
+
+    setTasks(updatedTask)
   }
 
   return (
